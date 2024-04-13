471d52869c09c0ff680cdc5c395c8081ff68cb3b
MilianCode <milian.code@gmail.com>
2024-04-13 11:02:32 +0200

    Init commit

---
+++ src/main/java/com/example/service/impl/BookServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.service.impl;
+
+import com.example.model.Book;
+import com.example.repository.BookRepository;
+import com.example.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
