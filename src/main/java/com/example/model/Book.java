c88a80ff0659d6b5a7b3cc1e01954e7220986668
MilianCode <milian.code@gmail.com>
2024-04-17 11:34:47 +0200

    Added book related code

--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -1,8 +1,8 @@
 package com.example.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.math.BigDecimal;
